3c6af34ffd9dc663e5801a99e42695aba1bbadac
dei0811 <deinergonzalezparedes12@gmail.com>
2026-05-24 19:24:57 -0500

    added apiurl

--- movietrack-admin/src/environments/environment.ts
+++ movietrack-admin/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://127.0.0.1:8000',
+  apiUrl: 'https://proyecto-movietrack-backend.onrender.com',
   tmdbImageUrl: 'https://image.tmdb.org/t/p/w500'
 };
